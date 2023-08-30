20f0d911ab311143b08ddf54672385049e131324
Pokepoison <jordyaw7@gmail.com>
2023-08-30 15:21:23 -0400

    updates code

---
+++ apiRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const dbFilePath = path.join(__dirname, 'develop', 'db', 'db.json');
+
+const getNotes = () => {
+  const data = fs.readFileSync(dbFilePath, 'utf8');
+  return JSON.parse(data);
+};
+
+const saveNotes = (notes) => {
+  fs.writeFileSync(dbFilePath, JSON.stringify(notes), 'utf8');
+};
+
+router.get('/notes', (req, res) => {
+  const notes = getNotes();
+  res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = getNotes();
+  notes.push(newNote);
+  saveNotes(notes);
+  res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const idToDelete = req.params.id;
+  const notes = getNotes().filter((note) => note.id !== idToDelete);
+  saveNotes(notes);
+  res.json({ message: 'Note deleted successfully' });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./apiRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('develop/public'));
+
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
