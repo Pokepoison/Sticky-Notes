5673799eb3ab9bfd0a6204f66713d55cf995d16d
Pokepoison <jordyaw7@gmail.com>
2023-09-14 21:43:24 -0400

    updates code

--- apiRoutes.js
+++ apiRoutes.js
@@ -3,35 +3,57 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const dbFilePath = path.join(__dirname, 'Develop', 'db', 'db.json');
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 const getNotes = () => {
-  const data = fs.readFileSync(dbFilePath, 'utf8');
-  return JSON.parse(data);
+  // const data = fs.readFileSync(dbFilePath, 'utf8');
+  // return JSON.parse(data);
+  return new Promise((resolve, reject) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(data) || []);
+    });
+  })
 };
 
 const saveNotes = (notes) => {
-  fs.writeFileSync(dbFilePath, JSON.stringify(notes), 'utf8');
-};
+  // fs.writeFileSync(dbFilePath, JSON.stringify(notes), 'utf8');
+  return new Promise((resolve, reject) => {
+    fs.writeFile(dbFilePath, JSON.stringify(notes), 'utf8', (err) => {
+      if (err) reject(err);
+      else resolve();
+    })
+})};
 
 router.get('/notes', (req, res) => {
-  const notes = getNotes();
-  res.json(notes);
+  getNotes().then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+    // const notes = getNotes();
+  // res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
   const newNote = req.body;
-  const notes = getNotes();
-  notes.push(newNote);
-  saveNotes(notes);
-  res.json(newNote);
-});
+  getNotes().then((notes) => {
+    notes.push(newNote);
+    saveNotes(notes).then(() => res.json(newNote));
+  }) .catch((err) => res.status(500).json(err));
+  });
+  // const notes = getNotes();
+  // notes.push(newNote);
+  // saveNotes(notes);
+  // res.json(newNote);
 
 router.delete('/notes/:id', (req, res) => {
   const idToDelete = req.params.id;
-  const notes = getNotes().filter((note) => note.id !== idToDelete);
-  saveNotes(notes);
-  res.json({ message: 'Note deleted successfully' });
-});
+  getNotes().then((notes) => {
+    const filteredNotes = notes.filter((note) => note.id !== idToDelete);
+    saveNotes(filteredNotes).then(() => res.json({ message: 'Note deleted successfully' }));
+  }) .catch((err) => res.status(500).json(err))
+  });
+  // const notes = getNotes().filter((note) => note.id !== idToDelete);
+  // saveNotes(notes);
+  // res.json({ message: 'Note deleted successfully' });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('develop/public'));
+app.use(express.static('public'));
+
+app.use(cors());
 
 app.use('/api', apiRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.use(cors());
